93d5e40f54054bd1cc39b164d26e9945d9d6243c
sergey_o <30532154+lazysergey@users.noreply.github.com>
2019-04-25 09:58:46 +0300

    Update post.ts

--- src/app/models/post.ts
+++ src/app/models/post.ts
@@ -1,6 +1,3 @@
-/**
- * @param { localMockOnly } - new posts that are uneditable as we need request api for post details, which is not updateable.
- */
 export class PostBase {
   title: string;
   body: string;
